f1660a36ad0f6ae22e283cb5a64a715f684cb606
s-PigaChavigny <sterenn.pigachavigny@gmail.com>
2026-09-18 14:16:31 +0200

    edit artistes

--- api/artistes/update.php
+++ api/artistes/update.php
@@ -1,2 +1,17 @@
 <?php
 
+require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
+require_once '../../functions/ctrlSaisies.php';
+
+$idArt = ($_POST['idArt']);
+$nomArt = ($_POST['nomArt']);
+$prenomArt = ($_POST['prenomArt']);
+$idGp = ($_POST['idGp']);
+if(empty($nomArt) || empty($prenomArt)){
+    header('Location: ../../views/backend/artistes/list.php');
+}
+
+
+sql_update('artiste', "idGp = $idGp", "idArt = $idArt");
+
+header('Location: ../../views/backend/artistes/list.php');
