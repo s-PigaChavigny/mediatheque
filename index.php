fec2e8d9fa01aa218e7995f82449104f6b5352f0
s-PigaChavigny <sterenn.pigachavigny@gmail.com>
2026-09-18 17:51:18 +0200

    acceuil

--- index.php
+++ index.php
@@ -4,9 +4,17 @@ sql_connect();
 
 
 ?>
-<br>
-<br>
-<h1>Bienvenue sur le site de la Médiathèque ! Nous sommes ravis de vous accueillir sur notre plateforme dédiée à la musique.</h1>
+<main style="margin-left: 50pt; margin-right: 50pt;">
+	<br>
+	<br>
+	<h1>Bienvenue sur le site de la Médiathèque ! </h1>
+    <h2>Nous vous accueillons sur notre plateforme dédiée à la gestion du catalogue musical.</h2>
+    <br>
+    <br>
+    <h4>L'interface d'administration vous permet de gérer efficacement les différents aspects de la collection musicale. Vous pouvez créer, modifier et supprimer les groupes, les artistes, les albums et les titres présents dans le catalogue municipal.</h4>
+    
+
+</main>
 
 
 <?php require_once 'footer.php'; ?>
