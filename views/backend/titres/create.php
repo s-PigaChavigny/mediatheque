41f99b4ddadc885d7e62f97e7c723c94f5365dc7
Margauuuxxx <margaux.daviaud@mmibordeaux.com>
2026-09-18 12:50:04 +0200

    titres ( create et list)

--- views/backend/titres/create.php
+++ views/backend/titres/create.php
@@ -1,3 +1,31 @@
 <?php
 include '../../../header.php';
 
+?>
+
+<!-- Bootstrap form to create a new statut -->
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Création nouveau titre</h1>
+        </div>
+        <div class="col-md-12">
+            <!-- Form to create a new statut -->
+            <form action="<?php echo ROOT_URL . '/api/titres/create.php' ?>" method="post">
+                <div class="form-group">
+                    <label for="NomTit">Nom du titre</label>
+                    <input id="NomTit" name="NomTit" class="form-control" type="text" autofocus="autofocus" />
+                </div>
+                <div class="form-group">
+                    <label for="dureeTit">Durée du titre</label>
+                    <input id="dureeTit" name="dureeTit" class="form-control" type="text" />
+                </div>
+                <br />
+                <div class="form-group mt-2">
+                    <a href="list.php" class="btn btn-moyen">List</a>
+                    <button type="submit" class="btn btn-clair">Confirmer create ?</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
--- views/backend/titres/list.php
+++ views/backend/titres/list.php
@@ -1,3 +1,41 @@
 <?php
 include '../../../header.php'; // contains the header and call to config.php
 
+//Load all titres from database
+$titres = sql_select("titre", "*");
+?>
+
+<!-- Bootstrap default layout to display all titres in foreach -->
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Titres</h1>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Id Titre</th>
+                        <th>Id Album</th>
+                        <th>Nom titre</th>
+                        <th>Durée titre</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach($titres as $titre) { ?>
+                        <tr>
+                            <td><?php echo($titre['idTit']); ?></td>
+                            <td><?php echo($titre['idAlb']); ?></td>
+                            <td><?php echo($titre['NomTit']); ?></td>
+                            <td><?php echo($titre['dureeTit']); ?></td>
+                        </tr>
+                </tbody>
+                                <a href="delete.php?idGp=<?php echo($titre['idTit']); ?>" class="btn btn-fonce">Delete</a>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<?php
+include '../../../footer.php'; // contains the footer
