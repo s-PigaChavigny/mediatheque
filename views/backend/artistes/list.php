b6b992aea7b03c537496ae33367c2e7bfc910008
s-PigaChavigny <sterenn.pigachavigny@gmail.com>
2026-09-18 12:44:10 +0200

    list artiste

--- views/backend/artistes/list.php
+++ views/backend/artistes/list.php
@@ -1,20 +1,23 @@
 <?php
 include '../../../header.php'; // contains the header and call to config.php
 
-//Load all artists from database
-$artistes = sql_select("artiste", "*");
+// Load artists with the name of their group
+$artistes = sql_select(
+    "ARTISTE INNER JOIN GROUPE ON ARTISTE.idGp = GROUPE.idGp",
+    "ARTISTE.idArt, ARTISTE.idGp, GROUPE.nomGp, ARTISTE.nomArt, ARTISTE.prenomArt"
+);
 ?>
 
 <!-- Bootstrap default layout to display all artists in foreach -->
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <h1>Groupes</h1>
+            <h1>Artistes</h1>
             <table class="table table-striped">
                 <thead>
                     <tr>
                         <th>Id Artiste</th>
-                        <th>Id Groupe</th>
+                        <th>Nom Groupe</th>
                         <th>Nom</th>
                         <th>Prénom</th>
                     </tr>
@@ -23,12 +26,12 @@ $artistes = sql_select("artiste", "*");
                     <?php foreach($artistes as $artiste) { ?>
                         <tr>
                             <td><?php echo($artiste['idArt']); ?></td>
-                            <td><?php echo($artiste['idGp']); ?></td>
-                            <td><?php echo($artiste['nomArt']); ?></td>
-                            <td><?php echo($artiste['prenomArt']); ?></td>
-                        </tr>
-                </tbody>
-                                <a href="delete.php?idGp=<?php echo($artiste['idArt']); ?>" class="btn btn-fonce">Delete</a>
+                            <td><?php echo htmlspecialchars($artiste['nomGp'], ENT_QUOTES, 'UTF-8'); ?></td>
+                            <td><?php echo htmlspecialchars($artiste['nomArt'], ENT_QUOTES, 'UTF-8'); ?></td>
+                            <td><?php echo htmlspecialchars($artiste['prenomArt'], ENT_QUOTES, 'UTF-8'); ?></td>
+                            <td>
+                                <a href="edit.php?idArt=<?php echo (int) $artiste['idArt']; ?>" class="btn btn-warning">Edit</a>
+                                <a href="delete.php?idArt=<?php echo (int) $artiste['idArt']; ?>" class="btn btn-danger">Delete</a>
                             </td>
                         </tr>
                     <?php } ?>
