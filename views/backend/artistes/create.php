7dc444f81b6cbfe4970f34bc101a4f4a3a620138
s-PigaChavigny <sterenn.pigachavigny@gmail.com>
2026-09-18 12:50:33 +0200

    create artist

--- views/backend/artistes/create.php
+++ views/backend/artistes/create.php
@@ -13,12 +13,24 @@ include '../../../header.php';
             <!-- Form to create a new statut -->
             <form action="<?php echo ROOT_URL . '/api/artistes/create.php' ?>" method="post">
                 <div class="form-group">
+                    <label for="prenomArt">Prénom de l'artiste</label>
+                    <input id="prenomArt" name="prenomArt" class="form-control" type="text" />
+                </div>
+                <div class="form-group">
                     <label for="nomArt">Nom de l'artiste</label>
                     <input id="nomArt" name="nomArt" class="form-control" type="text" autofocus="autofocus" />
                 </div>
                 <div class="form-group">
-                    <label for="prenomArt">Prénom de l'artiste</label>
-                    <input id="prenomArt" name="prenomArt" class="form-control" type="text" />
+                    <label for="idGp">Groupe</label>
+                    <select id="idGp" name="idGp" class="form-control">
+                        <option value="">Sélectionnez un groupe</option>
+                        <?php
+                        $groupes = sql_select("GROUPE", "*");
+
+                        foreach($groupes as $groupe) { ?>
+                            <option value="<?php echo $groupe['idGp']; ?>"><?php echo $groupe['nomGp']; ?></option>
+                        <?php } ?>
+                    </select>
                 </div>
                 <br />
                 <div class="form-group mt-2">
