e514fe5bf18f4e64889560a3d8f7dfb8a60bf916
s-PigaChavigny <sterenn.pigachavigny@gmail.com>
2026-09-18 15:34:28 +0200

    edit titre

--- api/titres/update.php
+++ api/titres/update.php
@@ -1,2 +1,17 @@
 <?php
 
+require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
+require_once '../../functions/ctrlSaisies.php';
+
+$idTit = ($_POST['idTit']);
+$nomTit = ($_POST['nomTit']);
+$dureeTit = ($_POST['dureeTit']);
+
+if(empty($nomTit) || empty($dureeTit)){
+    header('Location: ../../views/backend/titres/list.php');
+}
+
+
+sql_update('titre', "nomTit = '$nomTit', dureeTit = '$dureeTit'", "idTit = $idTit");
+
+header('Location: ../../views/backend/titres/list.php');
--- views/backend/titres/edit.php
+++ views/backend/titres/edit.php
@@ -1,3 +1,46 @@
 <?php
 include '../../../header.php';
 
+if(isset($_GET['idTit'])){
+    $idTit = $_GET['idTit'];
+    $nomTit = sql_select("titre", "nomTit", "idTit = $idTit")[0]['nomTit'];
+    $dureeTit = sql_select("titre", "dureeTit", "idTit = $idTit")[0]['dureeTit'];
+    $idAlb = sql_select("titre", "idAlb", "idTit = $idTit")[0]['idAlb'];
+}
+?>
+
+<!-- Bootstrap form to edit a statut -->
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Edition Titre</h1>
+        </div>
+        <div class="col-md-12">
+            <!-- Form to edit a  statut -->
+            <form action="<?php echo ROOT_URL . '/api/titres/update.php' ?>" method="post">
+                <div class="form-group">
+                    <label for="idTit">Id du titre</label>
+                    <input id="idTit" name="idTit" class="form-control" type="text" value="<?php echo($idTit); ?>" readonly />
+                    <label for="nomTit">Nom du titre</label>
+                    <input id="nomTit" name="nomTit" class="form-control" type="text" value="<?php echo($nomTit); ?>" />
+                    <label for="dureeTit">Durée du titre</label>
+                    <input id="dureeTit" name="dureeTit" class="form-control" type="text" value="<?php echo($dureeTit); ?>" />
+                    <label for="idAlb">Album</label>
+                    <select id="idAlb" name="idAlb" class="form-control">
+                        <option value="">Sélectionnez un album</option>
+                        <?php
+                        $albums = sql_select("ALBUM", "*");
+                        foreach($albums as $album) { ?>
+                            <option value="<?php echo $album['idAlb']; ?>" <?php if($album['idAlb'] == $idAlb) echo 'selected'; ?>><?php echo $album['nomA']; ?></option>
+                        <?php } ?>
+                    </select>
+                </div>
+                <br />
+                <div class="form-group mt-2">
+                    <a href="list.php" class="btn btn-primary">List</a>
+                    <button type="submit" class="btn btn-warning">Confirmer edit ?</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
