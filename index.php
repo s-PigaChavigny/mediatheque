079faa20906db7d8f35b7a6664341b50c466677d
s-PigaChavigny <sterenn.pigachavigny@gmail.com>
2026-09-18 11:31:51 +0200

    acceuil

--- index.php
+++ index.php
@@ -4,8 +4,9 @@ sql_connect();
 
 
 ?>
-
-
+<br>
+<br>
+<h1>Bienvenue sur le site de la Médiathèque ! Nous sommes ravis de vous accueillir sur notre plateforme dédiée à la musique.</h1>
 
 
 <?php require_once 'footer.php'; ?>
